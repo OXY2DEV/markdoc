eae96731b19c00924c944610c180e7b5ebe177c4
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-21 10:19:26 +0600

    chore(ci): Foxed typo

--- markdoc.sh
+++ markdoc.sh
@@ -4,6 +4,6 @@
 commands=`echo "$1" | jq -r 'to_entries[] | "pandoc \(.value | join(" ")) -t markdoc.lua -o \(.key)"'`
 
 while IFS= read -r line; do
-	echo $line
-end <<< $commands
+	echo "$line"
+end <<< "$commands"
 
