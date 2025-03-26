02b3b162093e8040644d33596e58381c86afffb7
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-26 15:46:28 +0600

    feat: Complete YAML metadata support

--- lua/nvim-markdoc/parsers/yaml.lua
+++ lua/nvim-markdoc/parsers/yaml.lua
@@ -1,16 +1,28 @@
 local yaml = {};
-
 local spec = require("nvim-markdoc.spec");
 
+--- Gets text from a YAML node(removes ', ")
+---@param node userdata
+---@param buffer integer
+---@return string
 local function get_node_text(node, buffer)
+	---|fS
+
 	local text = vim.treesitter.get_node_text(node, buffer)
 	text = text:gsub("^['\"]", "");
 	text = text:gsub("['\"]$", "");
 
 	return text;
+
+	---|fE
 end
 
+--- Turns string into other data types.
+---@param str string
+---@return number | boolean | string
 local function toval(str)
+	---|fS
+
 	if tonumber(str) then
 		return str;
 	elseif str == "true" or str == "false" then
@@ -18,9 +30,13 @@ local function toval(str)
 	else
 		return str;
 	end
+
+	---|fE
 end
 
 yaml.__block_quotes = function (buffer, node)
+	---|fS
+
 	local _value = node:field("value")[1];
 	if not _value or not _value:child(0) or not _value:child(0) then return end
 
@@ -54,6 +70,8 @@ yaml.__block_quotes = function (buffer, node)
 	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
 		block_quotes = _config
 	});
+
+	---|fE
 end
 
 yaml.__fold_refs = function (buffer, node)
@@ -69,6 +87,8 @@ yaml.__fold_markers = function (buffer, node)
 end
 
 yaml.__tags = function (buffer, node)
+	---|fS
+
 	local _value = node:field("value")[1];
 	if not _value or not _value:child(0) or not _value:child(0) then return end
 
@@ -101,9 +121,101 @@ yaml.__tags = function (buffer, node)
 	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
 		tags = _config
 	});
+
+	---|fE
 end
 
+yaml.__table = function (buffer, node)
+	---|fS
+
+	local _value = node:field("value")[1];
+	if not _value or not _value:child(0) or not _value:child(0) then return end
+
+	local value = _value:child(0);
+	local _config = {};
+
+	for option in value:iter_children() do
+		local opt_name = option:field("key")[1];
+		local opt_val  = option:field("value")[1];
+
+		if opt_name and opt_val then
+			if get_node_text(opt_name, buffer) ~= "col_minwidth" then
+				local _tags = {};
+
+				for entry in opt_val:child(0):iter_children() do
+					if entry:type() == "flow_node" then
+						table.insert(_tags, get_node_text(entry, buffer))
+					end
+				end
+
+				_config[get_node_text(opt_name, buffer)] = _tags;
+			else
+				_config[get_node_text(opt_name, buffer)] = tonumber(get_node_text(opt_val:child(0), buffer));
+			end
+		end
+	end
+
+	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
+		table = _config
+	});
+
+	---|fE
+end
+
+yaml.__textwidth = function (buffer, node)
+	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
+		textwidth = toval(get_node_text(node:field("value")[1], buffer))
+	});
+end
+
+yaml.__title = function (buffer, node)
+	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
+		title = toval(get_node_text(node:field("value")[1], buffer))
+	});
+end
+
+yaml.__title_tag = function (buffer, node)
+	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
+		title_tag = toval(get_node_text(node:field("value")[1], buffer))
+	});
+end
+
+yaml.__toc_title = function (buffer, node)
+	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
+		toc_tag = toval(get_node_text(node:field("value")[1], buffer))
+	});
+end
+
+yaml.__toc = function (buffer, node)
+	---|fS
+
+	local _value = node:field("value")[1];
+	if not _value or not _value:child(0) or not _value:child(0) then return end
+
+	local value = _value:child(0);
+	local _config = {};
+
+	for option in value:iter_children() do
+		local opt_name = option:field("key")[1];
+		local opt_val  = option:field("value")[1];
+
+		if opt_name and opt_val then
+			_config[get_node_text(opt_name, buffer)] = get_node_text(opt_val:child(0), buffer);
+		end
+	end
+
+	spec.config = vim.tbl_deep_extend("force", spec.config or spec.default, {
+		toc = _config
+	});
+
+	---|fE
+end
+
+------------------------------------------------------------------------------
+
 yaml.markdoc = function (buffer, node)
+	---|fS
+
 	local value = node:field("value")[1];
 
 	if not value then
@@ -117,10 +229,12 @@ yaml.markdoc = function (buffer, node)
 			goto continue;
 		end
 
-		vim.print(pcall(yaml["__" .. get_node_text(name, buffer)], buffer, option));
+		pcall(yaml["__" .. get_node_text(name, buffer)], buffer, option);
 
 	    ::continue::
 	end
+
+	---|fE
 end
 
 yaml.parse = function (buffer, TSTree)
@@ -137,8 +251,6 @@ yaml.parse = function (buffer, TSTree)
 			yaml.markdoc(buffer, capture_node);
 		end
 	end
-
-	vim.print(spec.config)
 end
 
 return yaml;
